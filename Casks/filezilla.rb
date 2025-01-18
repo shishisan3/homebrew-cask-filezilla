6c7c9af5bbb761c9287db71b849560428e92217a
Hidetaka Yokoi <hide@HidetakaMacBook-Air.local>
2025-01-18 21:35:13 +0900

    Add FileZilla Cask with correct SHA256 and URL

--- Casks/filezilla.rb
+++ Casks/filezilla.rb
@@ -1,8 +1,8 @@
 cask "filezilla" do
   version "3.68.1"
-  sha256 "e580fc27974125c0c762bb91f55ebe6651da019fa50086e93e229155098e3fc1"
+  sha256 "0ceeffd68816d46e905c286327592ee7999d3569842675526c1e95cbd7209bc9"
 
-  url "https://dl1.cdn.filezilla-project.org/client/FileZilla_#{version}_macos-x86.app.tar.bz2?h=wm32YiXI29iX7ADQSHphFw&x=1737031275"
+  url "https://raw.githubusercontent.com/shishisan3/homebrew-cask-filezilla/main/Casks/FileZilla_3.68.1_macos-x86.app.tar.bz2"
   name "FileZilla"
   desc "Cross-platform graphical FTP, FTPS, and SFTP client"
   homepage "https://filezilla-project.org/"
